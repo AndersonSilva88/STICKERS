5d3cf900eb1999393c4aeab490c4cc088230f785
Anderson Oliveira da Silva <anderson.os88@hotmail.com>
2023-03-27 19:56:34 -0300

    api filmes

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // CONEXAO HTTP E BUSCAR OS TOP 250 FILMES
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereco = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String body = response.body();
+
+        // EXTRAIR OS DADOS QUE INTERESSAM (TITULO, POSTER, CLASSIFICACAO)
+        JsonParse parser = new JsonParse();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        // EXIBIR E MANIPULAR OS DADOS
+        for (Map<String, String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+    }
+}
